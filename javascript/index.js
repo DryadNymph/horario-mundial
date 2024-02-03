7005937cc350d0ed7e0d418039d85f669d96bc86
DryadNymph <mymind203@yahoo.com>
2024-02-03 21:24:11 +0100

    Added JavaScript features

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
-alert("hello!");
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesDateElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[(<small>]A[</small>)]"
+  );
+}, 1000);
+setInterval(function () {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyDateElement.innerHTML = sydneyTime.format(
+    "h:mm:ss[(<small>]A[</small>)]"
+  );
+}, 1000);
